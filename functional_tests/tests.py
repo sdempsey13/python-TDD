6dfb4407bb9d2495cea2f82b67dc1632c568dd65
Meghan Sleeper <meghansleeper@Meghans-MacBook-Pro.local>
2019-01-30 14:20:40 -0800

    Added URL for creating lists via POST

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
@@ -83,7 +83,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        #She notices her list has a unique live_server_urle
+        #She notices her list has a unique URL
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists.+')
 
@@ -92,7 +92,7 @@ class NewVisitorTest(LiveServerTestCase):
         ##We use a new browser session to make sure that no information of Edith's
         ##is coming through from cookies, etc
         self.browser.quit
-        self.browser = webdriver.firefox()
+        self.browser = webdriver.Firefox()
 
         #Francis visits the home page. There is no sign of Edit's list
         self.browser.get(self.live_server_url)
