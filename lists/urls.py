ca06ef21f6c7171cba74786c5e66f67d4e32e5a5
Meghan Sleeper <meghansleeper@Meghans-MacBook-Pro.local>
2019-02-27 17:11:09 -0800

    URLs regex set to accept more than 9 items and use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
-    url(r'^(/[0-9]+/)/$', views.view_list, name='view_list'),
-    url(r'^(/[0-9]+/)/add_item$', views.add_item, name='add_item'),
+    url(r'^([0-9]+)/$', views.view_list, name='view_list'), # Change \d to r'^([0-9]+)$' to capture numbers greater than 9
+    url(r'^([0-9]+)/add_item$', views.add_item, name='add_item'), # Change \d to r'^([0-9]+)$' to capture numbers greater than 9
 ]
