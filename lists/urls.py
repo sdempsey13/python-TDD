55c0df0856fbc482f0a30b59328ace18a1c63b7d
Meghan Sleeper <meghansleeper@Meghans-MacBook-Pro.local>
2019-02-27 16:15:44 -0800

    changed urls to accept more than 9 lists

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
-    url(r'^(\d)/$', views.view_list, name='view_list'),
-    url(r'^(\d)/add_item$', views.add_item, name='add_item'),
+    url(r'^(/[0-9]+/)/$', views.view_list, name='view_list'),
+    url(r'^(/[0-9]+/)/add_item$', views.add_item, name='add_item'),
 ]
